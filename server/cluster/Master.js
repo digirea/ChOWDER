171763fafbd75f06bdb10ed71c4a00d5d1533dda
sokayama <shuhei-o@digirea.com>
2020-12-01 15:42:01 +0900

    add workerCount setting

--- server/cluster/Master.js
+++ server/cluster/Master.js
@@ -1,14 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
 class Master{
     constructor(cluster){
         this.cluster = cluster;
     }
 
     run(){
-        // const THREAD_MAX = require("os").cpus().length;
-        const THREAD_MAX = 4;
+        /* workerの数は、setting.jsonを参照する。なければcpu数 */
+        const settings = this.getSettings();
+        const WORKER_COUNT = settings.hasOwnProperty('workerCount') ? settings.workerCount : require("os").cpus().length;
 
         console.log("Master running");
-        for(let i=0;i<THREAD_MAX;i++){ // CPU数だけworkerをつくる
+        console.log("WORKER_COUNT: ",WORKER_COUNT);
+        for(let i=0;i<WORKER_COUNT;i++){ // CPU数だけworkerをつくる
             this.cluster.fork();
         }
         this.cluster.on("exit",(worker)=>{ // workerが死んだら新しいworkerをつくる
@@ -19,7 +24,7 @@ class Master{
         for(const id in this.cluster.workers){
             /* いずれかのworkerから受け取ったmessageを全workerに知らせる */
             this.cluster.workers[id].on("message",(msg)=>{
-                // console.log("[master]broadcastWorkers");
+                // console.log("[master]11111broadcastWorkers");
                 if(msg.method){
                     this.broadcastWorkers(msg);
                 }
@@ -30,6 +35,18 @@ class Master{
     broadcastWorkers(msg){
         for(const id in this.cluster.workers){
             this.cluster.workers[id].send(msg);
+        }
+    }
+
+    getSettings(){
+        const data = fs.readFileSync(path.join(__dirname, ".." ,'setting.json'),"utf-8");
+        try {
+            const settings = JSON.parse(String(data));
+            return settings;
+        } catch (e) {
+            console.error(e);
+            console.error("Failed to load setting.json");
+            process.exit(-1);
         }
     }
 }
